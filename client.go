69565076759da6eb8d8e05890d1a41a46fd08326
Ade <falaseade@gmail.com>
2025-09-21 18:08:39 +0100

    Modified the StartClient method to read and write data more efficiently

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/gorilla/websocket"
@@ -11,20 +12,50 @@ var dialer = websocket.Dialer{
 	WriteBufferSize: 1024,
 }
 
-func StartClient(url string) {
-	conn, _, err := dialer.Dial(url, nil)
+func StartClient(ctx context.Context, url string) {
+	log.Println("WebSocket client connecting to:", url)
+	conn, _, err := dialer.DialContext(ctx, url, nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		log.Printf("Failed to dial websocket: %v", err)
 		return
 	}
 	defer conn.Close()
+	log.Println("WebSocket client connected.")
 
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("read error:", err)
-			break
-		}
-		log.Printf("Message Type: %d recv: %s", messageType, p)
-	}
+	msgChan := make(chan []byte)
+    errChan := make(chan error, 1)
+
+    go func() {
+		defer close(msgChan)
+        defer close(errChan)
+        for {
+            _, p, err := conn.ReadMessage()
+            if err != nil {
+                errChan <- err
+                return
+            }
+            msgChan <- p
+        }
+    }()
+for {
+        select {
+			case <-ctx.Done():
+            log.Println("Shutdown signal received, closing WebSocket client.")
+            err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+            if err != nil {
+                log.Println("Error during websocket close:", err)
+            }
+            return
+			
+			case msg := <-msgChan:
+            log.Printf("Received message: %s", msg)
+
+			case err := <-errChan:
+            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
+                log.Printf("Error reading from websocket: %v", err)
+            }
+            log.Println("WebSocket connection closed.")
+            return
+        }
+    }
 }
