9ff0e2ec4ec9c7e7d6f9e5235358c49cd6f1d50c
Ade <falaseade@gmail.com>
2025-09-21 17:48:23 +0100

    Created jetsream config file to hold details

---
+++ config/jetstreamConfig.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+
+
+func SetupJetstreamConfig()(jetstream.StreamConfig, error){
+	streamName := os.Getenv("JS_STREAM_NAME")
+	if streamName == "" {
+		return jetstream.StreamConfig{}, errors.New("env var JS_STREAM_NAME is required")
+	}
+	streamSubject := os.Getenv("JS_STREAM_SUBJECTS")
+	if streamSubject == "" {
+		return jetstream.StreamConfig{}, errors.New("env var JS_STREAM_SUBJECT is required")
+	}
+	retentionHours, err := strconv.Atoi(os.Getenv("JS_RETENTION_HOURS"))
+	if err != nil {
+		return jetstream.StreamConfig{}, errors.New("env var JS_RETENTION_HOURS must be a valid integer")
+	}
+
+	jetstreamConfig := jetstream.StreamConfig {
+		Name: streamName,
+		Subjects: []string{streamSubject},
+		MaxAge: time.Duration(retentionHours) * time.Hour,
+	}
+	
+	return jetstreamConfig, nil
+}
+
