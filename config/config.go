65b16b7e00a48274bc76d9cb6e8f19d6c3236e90
Ade <falaseade@gmail.com>
2025-11-15 22:40:24 +0000

    config changes to integrate symbols for building url

--- config/config.go
+++ config/config.go
@@ -3,16 +3,28 @@ package config
 import (
 	"errors"
 	"os"
+	"strings"
 )
 
 type Config struct {
 	WebhookURL string
 	NatsUrl string
+	Symbols []string
 }
 
 func SetupConfig()(Config, error){
 	webhookUrl := os.Getenv("WEBHOOK_URL")
 	natsUrl := os.Getenv("NATS_URL")
+	symbolsString := os.Getenv("SYMBOLS")
+	if symbolsString == "" {
+		return Config{}, errors.New("environment variable SYMBOLS is not set, must be set")
+	}
+	var symbols []string
+	for s := range strings.SplitSeq(symbolsString, ",") {
+        symbols = append(symbols, strings.ToUpper(strings.TrimSpace(s)))
+	}
+	
+	
 	if webhookUrl == "" {
 		return Config{}, errors.New("environment variable WEBHOOK_URL is not set, must be set")
 	}
@@ -22,6 +34,7 @@ func SetupConfig()(Config, error){
 	config := Config{
 		WebhookURL: webhookUrl,
 		NatsUrl: natsUrl,
+		Symbols: symbols,
 	}
 	return config, nil
 }
